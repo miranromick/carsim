8ab22550d37b14f3920e57e08a2f27d0c34c99f5
learn <learn@learn18.local>
2017-04-27 17:13:47 -0700

    second commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express')
 
 // requiring our new body-parser
 var bodyParser = require('body-parser')
+var fs = require('fs')
+var Car = require('./car')
+
 
 var app = express()
 app.set('view engine', 'ejs')
@@ -19,12 +22,53 @@ app.post('/status', function(request, response){
   //because we're using body-parser, incomming form data
   //is found on request.body
   let responses = request.body
-  console.log(response)
+  let carStats = request.body
+  let newMake = request.body.make
+  let newYear = request.body.year
+  let newColor = request.body.color
+  let newSpeed = request.body.speed
+  let newLight = request.body.light
+  let newParkingBrake = request.body.parkingBrake
+  
+
+  let newCar = new Car(newMake, newYear, newColor)
+  newCar.carMakes()
+  newCar.save()
+
 
   //In this case, we're just turning around and dispaying the user input
-  response.render('status', responses )
+  response.render('status', { 'car': newCar } )
 });
 
+
+app.get('/accelerate', function(request,response){
+  let newCar = new Car('a','a')
+  newCar.load()
+  newCar.accelerate()
+  newCar.save()
+  response.render('status', {'car': newCar} )
+})
+app.get('/brake', function(request,response){
+  let newCar = new Car('a','a')
+  newCar.load()
+  newCar.brake()
+  newCar.save()
+  response.render('status', {'car': newCar} )
+})
+app.get('/light', function(request,response){
+  let newCar = new Car('a','a')
+  newCar.load()
+  newCar.lights()
+  newCar.save()
+  response.render('status', {'car': newCar} )
+})
+app.get('/parkingBrake', function(request,response){
+  let newCar = new Car('a','a')
+  newCar.load()
+  newCar.parkingBrakes()
+  newCar.save()
+  response.render('status', {'car': newCar} )
+})
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-});
+})
--- car.js
+++ car.js
@@ -1,20 +1,97 @@
-var fs = required('fs')
+var fs = require('fs')
+
+
 
 
 class Car {
-  constructor() {
+  constructor(carMake, carYear, carColor) {
     this.make = carMake,
     this.year = carYear,
-    this.speed = 0
+    this.color = carColor,
+    this.speed = 0,
+    this.light = "off",
+    this.parkingBrake ="off"
+    this.img = undefined
   }
+  carMakes(){
+    if(this.make == "jeep"){
+      this.img = "http://www.pngpix.com/wp-content/uploads/2016/06/PNGPIX-COM-Strut-Jeep-Wrangler-SUV-PNG-Image.png"
+    }
+    else if(this.make == "bentley"){
+      this.img = "http://www.pngpix.com/wp-content/uploads/2016/06/PNGPIX-COM-Black-Bentley-Continental-GT-V8-Car-PNG-Image.png"
+    }
+  else if(this.make == "tesla"){
+    this.img = "http://www.pngpix.com/wp-content/uploads/2016/06/PNGPIX-COM-Tesla-Model-S-Red-Car-PNG-Image.png"
+  }
+  else if(this.make == "porsche"){
+    this.img = "http://www.pngpix.com/wp-content/uploads/2016/06/PNGPIX-COM-Red-Porsche-911-GT3-RS-4-Car-PNG-Image.png"
+  }
+  else if(this.make == "aston martin"){
+    this.img = "http://www.pngpix.com/wp-content/uploads/2016/06/PNGPIX-COM-Aston-Martin-Vanquish-Car-PNG-Image.png"
+  }
+  else if(this.make == "honda"){
+    this.img = "http://pngimg.com/uploads/honda/honda_PNG10341.png"
+  }
+  else{
+    this.img =""
+  }
+}
+  accelerate(){
+      if(this.parkingBrake == "off"){
+        this.speed += 10
+      }else{
+        this.speed = 0
+      }
+  }
+  brake(){
+    if (this.speed <= 7){
+      this.speed = 0
+    }
+    else {
+      this.speed -= 7
+    }
+  }
+  lights(){
+    if (this.light == "off"){
+      this.light = "on"
+    }
+    else{
+      this.light = "off"
+    }
+  }
+  parkingBrakes(){
+      if(this.parkingBrake=="on"){
+        this.parkingBrake="off"
+      }else if (this.speed >=1) {
+        this.parkingBrake = "off"
+      }
+      else{
+        this.parkingBrake="on"
+      }
+  }
+  // colors(){
+  //   if(this.color == "blue"){
+  //     this.color = "blue"
+  //   }
+  //   else if(this.color == "red"){
+  //     this.color = "red"
+  //   }else {
+  //     this.color = "white"
+  //   }
+  // }
+
+
   save(){
     var tempCar ={
       make: this.make,
       year: this.year,
-      speed: this.speed
+      speed: this.speed,
+      light: this.light,
+      parkingBrake: this.parkingBrake,
+      color: this.color
     }
     fs.writeFileSync('data.json', JSON.stringify
-    ({make: this.make, year: this.year, speed: this.speed}))
+    (tempCar))
   }
     load(){
       var rawFile = fs.readFileSync('data.json')
@@ -22,6 +99,9 @@ class Car {
       this.make = tempCar.make
       this.year = tempCar.year
       this.speed = tempCar.speed
+      this.light = tempCar.light
+      this.parkingBrake = tempCar.parkingBrake
+      this.color = tempCar.color
     }
 }
 
